cec2b270ea87a980309ae011b61fd6d5c60816ac
Mark van der Loo <mark.vanderloo@gmail.com>
2019-07-30 11:48:46 +0200

    validation report

--- STS/validate.R
+++ STS/validate.R
@@ -1,3 +1,13 @@
+#  Demo: STS data validation rules
+#  Mark van der Loo, Olav ten Bosch, Dick Windmeijer
+#  Statistics Netherlands, August 2019
+#
+# To install the packages used here see:
+# https://github.com/data-cleaning/validate
+# https://github.com/data-cleaning/validatereport
+# https://github.com/SNStatComp/GenericValidationRules
+
+
 # Load a function to read the transmission file format.
 source("STS/stsfun.R")
 
@@ -13,13 +23,21 @@ sts <- read_sts("STS/data/STSIND_TOVT_Q_2018_0002_V0001.csv")
 rls <- validator(.file="STS/rules/sts_rules.yaml")
 
 # confront data with rules
-out <- confront(sts$data, rls, ref=list(meta=sts$meta))
+out <- confront(sts$data, rls, ref=list(meta=sts$meta)
+                , key = names(sts$data)[1:7])
 
 # study result
 summary(out)
 
 # turn result into regular dataset
 validation_data <- as.data.frame(out)
+View(validation_data)
+
+# Translate results into ESS validation report format.
+# validatereport (version >= 0.5.0)
+library(validatereport)
+
+export_ess_validation_report(out, rls, file="STS/STS_validation_report.json")
 
 
 
